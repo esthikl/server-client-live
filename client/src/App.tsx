c71666abe8fd3519e7789e3e834985b3993d85df
Esther Kleinhenz <esther.kleinhenz@p-perior.com>
2023-02-26 15:06:54 +0100

    #6 fetching data from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {counter: [] as string[]}
+
+  componentDidMount() {
+    fetch('http://localhost:5000/')
+      .then(res => {
+        console.log(res);
+        return res.json()
+      })
+      .then(counter => {
+        console.log(counter);
+        this.setState({ counter })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>happyhippo</h1>
+        {this.state.counter}
+      </div>
+    );
+  }
 }
 
 export default App;
