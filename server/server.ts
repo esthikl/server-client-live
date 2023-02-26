1da7d7d25db62e561f3ebd07b7311dffdef87017
Esther Kleinhenz <esther.kleinhenz@p-perior.com>
2023-02-26 16:12:27 +0100

    #9 added counter to server

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,47 @@
-import express from 'express'
-import { increase } from './counter'
-
+import express, { Router } from 'express'
+import { setInterval } from 'timers'
 
 const app = express()
+const bodyParser = require('body-parser')
 const port = 5000
+
+var cors = require('cors')
+
+app.use(cors())
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', (_, res) => {
-    res.json(increase)
+    console.log(response)
+    res.json(response)
 })
 
 app.listen(port, () => console.log(`Running on port ${port}`))
+
+
+//Anwendung Counter
+
+let i: number = 1
+let response: string[] = []
+let iText: string
+
+function start() {
+    setInterval(increase, 1000)
+}
+
+setTimeout(start, 5000)
+
+export function increase() {
+    if (i < 300) {
+        i++
+        iText = String(i)
+        response.push(iText)
+    }
+    console.log(response)
+    return (response)
+}
